58446402718d19168b68287ee6f027696d46e612
Brian Lou <blou@princeton.edu>
2022-09-20 17:39:32 -0400

    Update code

--- run_full_colmap_nerf.sh
+++ run_full_colmap_nerf.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-DATASET_PATH=/scratch/gpfs/blou/Colgate/Sphere2
-EXPERIMENT_NAME=sphere2
-ITERATIONS=2000
+DATASET_PATH=/scratch/gpfs/blou/Colgate/Sphere2_Cropped
+EXPERIMENT_NAME=sphere2_cropped
+ITERATIONS=200000
+CONFIG=configs/config_sphere.txt
 
 module purge
 module load anaconda3/2021.5
 conda activate nerf
 
-:start 
 
+preprocess(){
 singularity exec --nv /scratch/gpfs/$USER/colmap_latest.sif colmap feature_extractor \
    --database_path $DATASET_PATH/database.db \
    --image_path $DATASET_PATH/images \
@@ -30,7 +31,12 @@ singularity exec --nv /scratch/gpfs/$USER/colmap_latest.sif colmap mapper \
     --database_path $DATASET_PATH/database.db \
     --image_path $DATASET_PATH/images \
     --output_path $DATASET_PATH/sparse
-    
+
+singularity exec --nv /scratch/gpfs/blou/colmap_latest.sif colmap bundle_adjuster \
+    --input_path $DATASET_PATH/sparse/0 \
+    --output_path $DATASET_PATH/sparse/0 \
+    --BundleAdjustment.refine_principal_point 1
+
 singularity exec --nv /scratch/gpfs/blou/colmap_latest.sif colmap model_converter \
     --input_path $DATASET_PATH/sparse/0 \
     --output_path $DATASET_PATH/sparse/0 \
@@ -39,15 +45,19 @@ singularity exec --nv /scratch/gpfs/blou/colmap_latest.sif colmap model_converte
 python utils.py \
     --operation remove_images \
     --datadir $DATASET_PATH
+}
 
-if [ ! -e $DATASET_PATH/database.db ]
-then
+preprocess
+
+
+while [ ! -f "$DATASET_PATH/database.db" ]
+do
     echo "Database not found, re-running COLMAP"
-    goto start
-fi
+    preprocess
+done
 
 singularity exec --nv /scratch/gpfs/$USER/tf_colmap_latest.sif python /scratch/gpfs/$USER/LLFF/imgs2poses.py $DATASET_PATH
 
-python run_nerf.py --config config_sphere.txt --expname $EXPERIMENT_NAME --datadir $DATASET_PATH --iterations $ITERATIONS --i_weights 500 
+python run_nerf.py --config $CONFIG --expname $EXPERIMENT_NAME --datadir $DATASET_PATH --iterations $ITERATIONS
 
 python gen_video.py --expname $EXPERIMENT_NAME --iterations $ITERATIONS --datadir $DATASET_PATH
--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
+import glob
 import os
 import sqlite3
 import numpy as np
 import shutil
+import cv2
 
 import configargparse
 
-def sql():
+def sql(args):
     
     dbPath = f"{args.datadir}/database.db"
     params = np.genfromtxt(args.calib, delimiter=",")
@@ -42,8 +44,22 @@ def edit_images(basedir):
     if count != 0:
         shutil.rmtree(os.path.join(basedir, "sparse"))
         os.remove(os.path.join(basedir, "database.db"))
-    print("Re-running COLMAP Next (by deleting database.db)")
+        print("Re-running COLMAP Next (by deleting database.db)")
 
+def crop_images(basedir):
+    print("Cropping")
+    # images = []
+    # images_path = os.path.join(basedir, "images/left")
+    # for filename in os.listdir(images_path):
+    #     img = cv2.imread(os.path.join(images_path,filename))
+    #     if img is not None:
+    #         cv2.imwrite(os.path.join(images_path,filename),img[:,0:-10])
+    
+    images_path = os.path.join(basedir, "images/right")
+    for filename in os.listdir(images_path):
+        img = cv2.imread(os.path.join(images_path,filename))
+        if img is not None:
+            cv2.imwrite(os.path.join(images_path,filename),img[:,0:-10])
 
 
 
@@ -51,13 +67,15 @@ def main():
     ps = configargparse.ArgumentParser()
     ps.add_argument("--datadir", type=str, help='data directory')
     ps.add_argument("--calib", type=str, help='calibration file')
-    ps.add_argument("--operation", type=str, choices=['calib','remove_images'],help='operation')
+    ps.add_argument("--operation", type=str, choices=['calib','remove_images','crop'],help='operation')
     args = ps.parse_args()
 
     if args.operation == "remove_images":
         edit_images(args.datadir)
     elif args.operation == "calib":
-        sql()
+        sql(args)
+    elif args.operation == "crop":
+        crop_images(args.datadir)
 
 if __name__ =='__main__':
     main()
